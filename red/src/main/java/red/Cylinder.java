cd583f02492d5756f6e3a45aa31618a83f1bd37c
MrT98 <toan.vp98.wapmaster@gmail.com>
2020-05-05 14:31:55 +0700

    refactor

--- red/src/main/java/red/Cylinder.java
+++ red/src/main/java/red/Cylinder.java
@@ -1,21 +1,18 @@
 package red;
 
 public class Cylinder {
-    public Cylinder() {
-    }
-
-    /**
-     * Says hello to the world.
-     * 
-     * @param args The arguments of the program.
-     */
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
-    public static double getVolume(int radius, int height){
-        double baseArea  = Math.PI * radius * radius;
-        double perimeter = 2 * Math.PI  * radius;
+    public static double getVolume(int radius, int height) {
+        double baseArea  = getBaseArea(radius);
+        double perimeter = getPerimeter(radius);
         double volume    = perimeter * height + 2 * baseArea;
         return volume;
+    }
+
+    private static double getPerimeter(int radius) {
+        return 2 * Math.PI * radius;
+    }
+
+    private static double getBaseArea(int radius) {
+        return Math.PI * radius * radius;
     }
 }
